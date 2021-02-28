ea4929b6461a905e99eb055f40a0a3fe15ab6e4a
Thom Williams <thomwilliams1990@gmail.com>
2021-02-28 19:34:23 +0000

    JS: if statements for character options defined

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
-  var lowerCase = "abcdefghiji";
-  var upperCase = "ABCDEF";
-  var numbers = "12345678";
-  var specialCharacter = "()*(&^%";
+function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var specialCharacter = " !#$%&'()*+,-./:;<=>?@][^_`{|}~";
+
+  var characterOptions = "";
+  var finalPassword = "";
 
   var lengthOfPassword = parseInt(prompt("How long would you like your password to be?"))
 
-  if (lengthOfPassword < 8){
+  if (lengthOfPassword < 8) {
     alert("Please select a length greater than 8");
-    return; 
+    return;
   }
-  if (lengthOfPassword > 128)
-  alert ("Please select shorter number of characters");
-  return;
 
+  if (lengthOfPassword > 128) {
+    alert("Please select shorter number of characters");
+    return;
+  }
 
+  var useUppercase = confirm("would you like to use upper case letters?")
+  if (useUppercase === true) {
+    characterOptions += upperCase
+  }
 
+  var useLowercase = confirm("Would you like to use any lowercase?")
+  if (useLowercase === true) {
+    characterOptions += lowerCase
+  }
 
+  var useNumbers = confirm("Would you like to use any numbers?")
+  if (useNumbers === true) {
+    characterOptions += numbers
+  }
 
-return "password"
+  var usespecialCharacter = confirm("Would you like to use any special characters?")
+  if (usespecialCharacter === true) {
+    characterOptions += specialCharacter
+  }
+
+  for (var i = 0; i <= lengthOfPassword; i++) {
+    finalPassword += characterOptions[Math.floor(Math.random() * characterOptions.length)]
+  }
+  return finalPassword;
 }
 
 
@@ -45,12 +69,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-/* event listener paired with button in the HTML - 
+/* event listener paired with button in the HTML -
 
 1st argument is click, second is write password
 
 
-/* 
+/*
 
 
 
